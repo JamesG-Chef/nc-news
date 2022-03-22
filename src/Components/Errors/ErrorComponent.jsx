d67b55ba2ec61b3ffbf8b8d9de14621cf31d2488
James_G <jamesg.chef@gmail.com>
2022-03-22 12:27:59 +0000

    Amend responsive styling issue to error page

--- src/Components/Errors/ErrorComponent.jsx
+++ src/Components/Errors/ErrorComponent.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import errorImg from '../../Images/errorImg.png'
-import '../../Styles/ErrorComponent.css'
+import React from "react";
+import errorImg from "../../Images/errorImg.png";
+import "../../Styles/ErrorComponent.css";
 
 const ErrorComponent = ({ error }) => {
-    return (
-        <div className="error_container">
-            <span className="error-text">Oops! we got a {error.status} {error.statusText}</span>
-            <div className="image-container">
-            <img src={errorImg} alt='Error' className="error_img" />
-            </div>
-            
+  return (
+    <div className="page_container">
+      <div className="error_container">
+        <span className="error-text">
+          Oops! we got a {error.status} {error.statusText}
+        </span>
+        <div className="image-container">
+          <img src={errorImg} alt="Error" className="error_img" />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ErrorComponent;
